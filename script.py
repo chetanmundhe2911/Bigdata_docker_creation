60a80724b65b6b8416fa45c65d2a688e6c2d489c
Chetan <67324853+chetanmundhe2911@users.noreply.github.com>
2025-01-20 19:52:16 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Load the data
+data = pd.read_csv("data/random_data.csv")
+
+
+# Print summary
+print("Data Summary:")
+print(data.describe())
+
+# Show a sample
+print("\nSample Data:")
+print(data.head())
